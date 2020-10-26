cca3e929cd836d6eafc7e993aa0bde451fe87c6b
frozar <fabien.rozar@gmail.com>
2020-10-26 17:18:25 +0100

    Retrieve data from lelscan in js.

--- src/google_cloud_function/index.js
+++ src/google_cloud_function/index.js
@@ -1,24 +1,75 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-'use strict';
+"use strict";
 
 // [START functions_helloworld_http]
-const escapeHtml = require('escape-html');
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
+const axios = require("axios");
+const { parse } = require("node-html-parser");
 
-// [END functions_helloworld_http]
+async function getIdxChapters(mangaURL) {
+  const data = await axios
+    .get(mangaURL)
+    .then(function (response) {
+      // console.log("response");
+      // console.log(response.data);
+      return response.data;
+    })
+    .catch(function (error) {
+      // res.send(error);
+      return error;
+    });
+
+  const root = parse(data);
+  // console.log("root");
+  const selectChapters = root
+    .querySelector("#header-image")
+    .querySelectorAll("select")[0];
+  const chapters = selectChapters.querySelectorAll("option");
+  // console.log(chapters.slice(0, 1));
+  // console.log(chapters[0].childNodes[0]);
+  const idxChapters = chapters.map((opt) => opt.childNodes[0].rawText);
+  return idxChapters;
+}
+
+async function getMangas() {
+  const data = await axios
+    .get("https://lelscan.net/lecture-en-ligne.php")
+    .then(function (response) {
+      // console.log("response");
+      // console.log(response.data);
+      return response.data;
+    })
+    .catch(function (error) {
+      // res.send(error);
+      return error;
+    });
+  // .then(function (data) {
+  // console.log(data);
+  const root = parse(data);
+  console.log("root");
+  const [selectChapters, selectMangas] = root
+    .querySelector("#header-image")
+    .querySelectorAll("select");
+  const chapters = selectChapters.querySelectorAll("option");
+  // console.log(chapters.slice(0, 1));
+  // console.log(chapters[0].childNodes[0]);
+  const idxChapter = chapters.map((opt) => opt.childNodes[0].rawText);
+  // console.log(idxChapter);
+
+  const mangas = selectMangas.querySelectorAll("option");
+  // console.log(mangas[0]);
+  const objMangas = mangas.map((opt) => {
+    return {
+      title: opt.childNodes[0].rawText,
+      URL: opt.rawAttrs.split("=")[1].split("'")[1],
+    };
+  });
+  // console.log(objMangas);
+  return objMangas;
+  // res.send("NOTHING\n");
+  // res.send(mangas);
+  // res.json(JSON.parse({ mangas, chapters }));
+  // });
+}
 
 // [START functions_helloworld_get]
 /**
@@ -31,146 +82,76 @@ const { exec } = require("child_process");
  * @param {Object} res Cloud Function response context.
  *                     More info: https://expressjs.com/en/api.html#res
  */
-exports.helloGET = (req, res) => {
-    exec("./toRun.sh", (error, stdout, stderr) => {
-      if (error) {
-        res.send(error.message);
-        return;
-    }
-    if (stderr) {
-        res.send(stderr);
-        return;
-    }
-    res.json(JSON.parse(stdout));
-  });
+exports.helloGET = async (req, res) => {
+  //   exec("./toRun.sh", (error, stdout, stderr) => {
+  //     if (error) {
+  //       res.send(error.message);
+  //       return;
+  //   }
+  //   if (stderr) {
+  //       res.send(stderr);
+  //       return;
+  //   }
+  //   res.json(JSON.parse(stdout));
+  // });
+  // res.send(`Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
+
+  // axios
+  //   // .get("https://lelscan.net/lecture-en-ligne-one-piece.php")
+  //   .get("https://lelscan.net/lecture-en-ligne.php")
+  //   .then(function (response) {
+  //     // console.log("response");
+  //     // console.log(response.data);
+  //     return response.data;
+  //   })
+  //   .catch(function (error) {
+  //     res.send(error);
+  //   })
+  //   .then(function (data) {
+  //     const root = parse(data);
+  //     // console.log("root");
+  //     const [selectChapters, selectMangas] = root
+  //       .querySelector("#header-image")
+  //       .querySelectorAll("select");
+  //     const chapters = selectChapters.querySelectorAll("option");
+  //     // console.log(chapters.slice(0, 1));
+  //     // console.log(chapters[0].childNodes[0]);
+  //     const idxChapter = chapters.map((opt) => opt.childNodes[0].rawText);
+  //     console.log(idxChapter);
+
+  //     const mangas = selectMangas.querySelectorAll("option");
+  //     // console.log(mangas[0]);
+  //     const objMangas = mangas.map((opt) => {
+  //       return {
+  //         title: opt.childNodes[0].rawText,
+  //         URL: opt.rawAttrs.split("=")[1].split("'")[1],
+  //       };
+  //     });
+  //     console.log(objMangas);
+  //     // res.send(mangas);
+  //     // res.json(JSON.parse({ mangas, chapters }));
+  //   });
+
+  let objMangas = await getMangas();
+  // console.log(objMangas);
+  console.log(objMangas[0].URL);
+
+  // const idxChapters = await getIdxChapters(objMangas[0].URL);
+  objMangas = await Promise.all(
+    objMangas.map(async (manga) => {
+      const idxChapters = await getIdxChapters(manga.URL);
+      // console.log(manga);
+      // console.log(idxChapters);
+      // const res = { ...manga, idxChapters };
+      // // console.log(res);
+      // return res;
+      return { ...manga, idxChapters };
+    })
+  );
+
+  console.log(objMangas);
+  // res.send("NOTHING\n");
+  res.send(objMangas);
+  // res.json(JSON.parse(objMangas));
 };
 // [END functions_helloworld_get]
-
-// [START functions_helloworld_http]
-/**
- * HTTP Cloud Function.
- *
- * @param {Object} req Cloud Function request context.
- *                     More info: https://expressjs.com/en/api.html#req
- * @param {Object} res Cloud Function response context.
- *                     More info: https://expressjs.com/en/api.html#res
- */
-exports.helloHttp = (req, res) => {
-  res.send(`Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
-};
-// [END functions_helloworld_http]
-
-// [START functions_helloworld_pubsub]
-/**
- * Background Cloud Function to be triggered by Pub/Sub.
- * This function is exported by index.js, and executed when
- * the trigger topic receives a message.
- *
- * @param {object} message The Pub/Sub message.
- * @param {object} context The event metadata.
- */
-exports.helloPubSub = (message, context) => {
-  const name = message.data
-    ? Buffer.from(message.data, 'base64').toString()
-    : 'World';
-
-  console.log(`Hello, ${name}!`);
-};
-// [END functions_helloworld_pubsub]
-
-// [START functions_helloworld_storage]
-/**
- * Generic background Cloud Function to be triggered by Cloud Storage.
- *
- * @param {object} file The Cloud Storage file metadata.
- * @param {object} context The event metadata.
- */
-exports.helloGCS = (file, context) => {
-  console.log(`  Event: ${context.eventId}`);
-  console.log(`  Event Type: ${context.eventType}`);
-  console.log(`  Bucket: ${file.bucket}`);
-  console.log(`  File: ${file.name}`);
-  console.log(`  Metageneration: ${file.metageneration}`);
-  console.log(`  Created: ${file.timeCreated}`);
-  console.log(`  Updated: ${file.updated}`);
-};
-// [END functions_helloworld_storage]
-
-/**
- * Background Cloud Function that throws an error.
- *
- * @param {object} event The Cloud Functions event.
- * @param {object} context The event metadata.
- * @param {function} callback The callback function.
- */
-
-exports.helloError = (event, context, callback) => {
-  // [START functions_helloworld_error]
-  // These WILL be reported to Stackdriver Error Reporting
-  console.error(new Error('I failed you'));
-  console.error('I failed you', new Error('I failed you too'));
-  throw new Error('I failed you'); // Will cause a cold start if not caught
-
-  // [END functions_helloworld_error]
-};
-
-/**
- * Background Cloud Function that throws a value.
- *
- * @param {object} event The Cloud Functions event.
- * @param {object} context The event metadata.
- * @param {function} callback The callback function.
- */
-/* eslint-disable no-throw-literal */
-
-exports.helloError2 = (event, context, callback) => {
-  // [START functions_helloworld_error]
-  // These will NOT be reported to Stackdriver Error Reporting
-  console.info(new Error('I failed you')); // Logging an Error object at the info level
-  console.error('I failed you'); // Logging something other than an Error object
-  throw 1; // Throwing something other than an Error object
-  // [END functions_helloworld_error]
-};
-/* eslint-enable no-throw-literal */
-
-/**
- * Background Cloud Function that returns an error.
- *
- * @param {object} event The Cloud Functions event.
- * @param {object} context The event metadata.
- * @param {function} callback The callback function.
- */
-/* eslint-disable */
-exports.helloError3 = (event, context, callback) => {
-  // This will NOT be reported to Stackdriver Error Reporting
-  // [START functions_helloworld_error]
-  callback('I failed you');
-  // [END functions_helloworld_error]
-};
-/* eslint-enable */
-
-/**
- * HTTP Cloud Function that returns an error.
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
-exports.helloError4 = (req, res) => {
-  // This will NOT be reported to Stackdriver Error Reporting
-  // [START functions_helloworld_error]
-  res.status(500).send('I failed you');
-  // [END functions_helloworld_error]
-};
-
-// [START functions_helloworld_template]
-const path = require('path');
-const pug = require('pug');
-
-// Renders the index.pug
-exports.helloTemplate = (req, res) => {
-  // Render the index.pug file
-  const html = pug.renderFile(path.join(__dirname, 'index.pug'));
-
-  res.send(html).end();
-};
-// [END functions_helloworld_template]
