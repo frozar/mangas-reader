854ec9da6f77e2fa7468cd0982ce243b8d352c5e
frozar <fabien.rozar@gmail.com>
2020-11-08 02:05:22 +0100

    Massive cleanup

--- src/components/DisplayImage.js
+++ src/components/DisplayImage.js
@@ -1,23 +1,13 @@
 import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
-import Paper, { styles } from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const BASE_URL = "https://lelscan.net/mangas";
 
 class DisplayImage extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.refImageFrame = React.createRef();
-  // }
-
   imageLoaded = () => {
     if (this.props.imageLoaded) {
       this.props.imageLoaded();
-    }
-    if (this.props.getRef) {
-      this.props.getRef(this.refImageFrame);
     }
   };
 
@@ -32,135 +22,24 @@ class DisplayImage extends React.Component {
     return `Chapter: ${idxChapter} - Scan: ${idxImage}`;
   }
 
-  // TODO: use the metarial theme style system
   render() {
     const { mangaInfo } = this.props;
-    // const offsetXProp = this.props.offsetX ? this.props.offsetX : 0;
-    // console.log({ offsetXProp }); className="card-container"
-    // console.log("DisplayImage: mangaInfo:", mangaInfo);
     return (
-      // <Box
-      //   style={{
-      //     // display: "inline-block",
-      //     // position: "relative",
-      //     // width: "100%",
-      //     // margin: "auto",
-      //     // // left: `${offsetXProp}px`,
-
-      //     margin: "auto",
-      //     display: "flex",
-      //     outline: "0",
-      //     // position: "relative",
-      //     // justifyContent: "center",
-      //     // flex-start | flex-end | center | space-between | space-around | space-evenly | start | end | left | right ... + safe | unsafe;
-      //     // justifyContent: "flex-start",
-      //     // justifyContent: "flex-end",
-      //     // justifyContent: "start",
-      //     // justifyContent: "end",
-      //     // justifyContent: "left",
-      //     // justifyContent: "right",
-      //     // justifyContent: "center",
-      //     // justifyContent: "space-between",
-      //     // justifyContent: "space-around",
-      //     // justifyContent: "space-evenly",
-      //   }}
-      //   // ref={this.refImageFrame}
-      // >
-      // <Paper
-      //   style={{
-      //     display: "inline-block",
-      //     // display: "table",
-      //     // margin: "2em auto",
-      //     // padding: "2em",
-      //     // backgroundColor: "#4e536b",
-      //     // border: "2px",
-      //     // borderStyle: "solid",
-      //     textAlign: "center",
-      //     // position: "absolute",
-      //     // top: 0,
-      //     // left: 0,
-      //     // position: "absolute",
-      //     // left: "0",
-      //     // right: "0",
-      //     // marginLeft: "auto",
-      //     // marginRight: "auto",
-      //     // // width: "90%",
-      //     // maxWidth: "-webkit-fill-available",
-      //     // justifyContent: "center",
-      //   }}
-      //   elevation={5}
-      //   // component="p"
-      // >
-      <TransitionGroup
-        style={
-          {
-            // position: "relative",
-            // top: 0,
-            // left: 0,
-            // position: "relative",
-            // display: "table-cell",
-            // height: "300px",
-            // textAlign: "center",
-            // width: "300px",
-            // verticalAlign: "middle",
-            // position: "relative",
-            // display: "block",
-            // textAlign: "center",
-            // // background: "yellow",
-            // // height: "909px",
-            // position: "absolute",
-            // left: "0",
-            // right: "0",
-            // marginLeft: "auto",
-            // marginRight: "auto",
-            // width: "100px",
-          }
-        }
-      >
+      <TransitionGroup>
         <CSSTransition
           key={this.getURL(mangaInfo)}
-          // key={mangaInfo.idxImage}
           timeout={300}
-          // classNames="slide"
           classNames="fade"
-          // ref={this.refImageFrame}
         >
-          {/* <div
-              style={
-                {
-                  // // position: "absolute", left: "50%"
-                  // position: "absolute",
-                  // left: "0",
-                  // right: "0",
-                  // // marginLeft: "auto",
-                  // // marginRight: "auto",
-                  // // width: "90%",
-                  // maxWidth: "-webkit-fill-available",
-                }
-              }
-            > */}
           <Tooltip title={this.tooltipTitle(mangaInfo)}>
             <img
               style={{
-                // display: "inline",
-                // // // position: "absolute",
-                // // // margin: "0 auto",
-                // position: "relative",
-                // // left: "-50%",
-                // // border: "dotted red 1px",
                 position: "absolute",
                 left: "0",
                 right: "0",
-                // marginLeft: "auto",
-                // marginRight: "auto",
-                // // // // width: "90%",
-                // // maxWidth: "-webkit-fill-available",
-                // // minWidth: "fit-content",
                 margin: "1em auto",
                 padding: "2em",
                 backgroundColor: "rgb(78, 83, 107)",
-                // position: "absolute",
-
                 // .MuiPaper-elevation-5
                 boxShadow:
                   "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 5px 8px 0px rgba(0,0,0,0.14), 0px 1px 14px 0px rgba(0,0,0,0.12)",
@@ -175,12 +54,9 @@ class DisplayImage extends React.Component {
               onLoad={this.imageLoaded}
               ref={this.props.innerRef}
             />
-            {/* </div> */}
           </Tooltip>
         </CSSTransition>
       </TransitionGroup>
-      // </Paper>
-      // </Box>
     );
   }
 }
--- src/components/ScanViewer.js
+++ src/components/ScanViewer.js
@@ -1,33 +1,19 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
-// import Slide from "@material-ui/core/Slide";
-// import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import { CSSTransition } from "react-transition-group";
 
 import DisplayImage from "./DisplayImage";
 // import WaitingScreen from "./WaitingScreen";
-// import {
-//   // discoverManga,
-//   // pingMangaDict,
-//   // previousImage,
-//   // nextImage,
-//   KEY_LAST_CHAPTER,
-// } from "../probe";
 
 function previousImage(mangaURL, idxChapter, idxImage, mangaDict) {
-  // console.log(mangaURL, idxChapter, idxImage, mangaDict);
   const chapters = mangaDict[mangaURL].chapters;
-  // const idxImageMax = chapters[idxChapter];
-  // console.log("idxImageMax", idxImageMax);
   if (0 < idxImage) {
     // Go to the previous image
     return [idxChapter, idxImage - 1];
   } else if (0 === idxImage) {
     const aChapters = Object.keys(chapters).sort();
     const idxInTabChapter = aChapters.indexOf(idxChapter);
-    // const idxInTabChapterMax = aChapters.length - 1;
     if (0 < idxInTabChapter) {
       // Go to the next chapter
       const idxPreviousChapter = aChapters[idxInTabChapter - 1];
@@ -43,11 +29,8 @@ function previousImage(mangaURL, idxChapter, idxImage, mangaDict) {
 }
 
 function nextImage(mangaURL, idxChapter, idxImage, mangaDict) {
-  // console.log(mangaURL, idxChapter, idxImage, mangaDict);
   const chapters = mangaDict[mangaURL].chapters;
   const idxImageMax = chapters[idxChapter] - 1;
-  // console.log("idxImage", idxImage);
-  // console.log("idxImageMax", idxImageMax);
   if (idxImage < idxImageMax) {
     // Go to the next image
     return [idxChapter, idxImage + 1];
@@ -76,11 +59,7 @@ class ScanViewer extends React.Component {
   initState = {
     idxChapter: null,
     idxImage: 0,
-    // displayedImage: false,
-    // offsetX: 0,
     errorMsg: "",
-    // action: "",
-    // in: false,
   };
   state = {
     mangaURL: "",
@@ -94,7 +73,6 @@ class ScanViewer extends React.Component {
         mangaURL: this.props.mangaURL,
         ...this.initState,
       });
-      // discoverManga(this.props.mangaURL, this.updateIdxLastChapter);
     } else if (this.state.initIdxChapter !== this.props.idxChapter) {
       this.setState({
         ...this.initState,
@@ -117,18 +95,14 @@ class ScanViewer extends React.Component {
 
   handleKeyDown = (evt) => {
     const { mangaURL, idxChapter, idxImage } = this.state;
-    // let action;
     let followingImageFct;
     if (evt.key === "ArrowLeft") {
-      // action = "PREVIOUS";
       followingImageFct = previousImage;
     } else if (evt.key === "ArrowRight") {
-      // action = "NEXT";
       followingImageFct = nextImage;
     }
 
     if (followingImageFct) {
-      // console.log("followingImage", followingImage);
       const res = followingImageFct(
         mangaURL,
         idxChapter,
@@ -140,114 +114,33 @@ class ScanViewer extends React.Component {
         this.setState({ idxChapter, idxImage });
       }
     }
-    // }
   };
 
   componentDidUpdate = () => {
     this.mayUpdateMangaURL();
-    // const { offsetX } = this.state;
-    // console.log({ offsetX });
-    // if (offsetX === null) {
-    // const { mangaURL, idxChapter, idxImage } = this.state;
-    // console.log("componentDidUpdate", { mangaURL, idxChapter, idxImage });
-    // probeImage(
-    //   "probe-current",
-    //   { mangaURL, idxChapter, idxImage },
-    //   this.setOffsetX
-    // );
-    // console.log("this.state", this.state);
-    // }
   };
 
-  // moveImage = (previousOrNextImage, errorMsg) => {
-  //   const { mangaURL, idxChapter, idxImage } = this.state;
-  //   let answer = previousOrNextImage(mangaURL, idxChapter, idxImage);
-  //   while (answer === "NOT_READY") {
-  //     answer = previousOrNextImage(mangaURL, idxChapter, idxImage);
-  //   }
-  //   if (answer === "NO_IMAGE") {
-  //     this.setState({ errorMsg });
-  //   } else if (typeof answer === "object") {
-  //     this.setState({
-  //       ...answer,
-  //       displayedImage: false,
-  //       offsetX: 0,
-  //       // action: "",
-  //     });
-  //     window.scrollTo(0, 0);
-  //   }
-  // };
-
-  // handleOnExited = () => {
-  //   this.setState({ offsetX: 0 });
-  //   window.scrollTo(0, 0);
-  // };
-
+  /**
+   * Compute the position of the DisplayImage component to adjust the
+   * scroll behavior to its top.
+   */
   imageLoaded = () => {
-    // this.setState({
-    //   displayedImage: true,
-    // });
-    // window.scrollTo(0, 0);
-    // window.scrollTo({ top: 0, behavior: "smooth" });
-    console.log("this.refImageFrame");
-    console.log(this.refImageFrame);
-    // Compute the position of the DisplayImage component to adjust the
-    // scroll behavior to its top.
     const bodyRect = document.body.getBoundingClientRect(),
       elemRect = this.refImageFrame.current.getBoundingClientRect(),
-      offset = elemRect.top - bodyRect.top;
-    console.log("offset", offset);
-    window.scrollTo({ top: offset, behavior: "smooth" });
-    // console.log("Should scroll");
-    // const { mangaURL, idxChapter, idxImage } = this.state;
-    // // I - Discover the current and sibling chapter if not done
-    // pingMangaDict(mangaURL, idxChapter, idxImage);
-    // // II - Probe the next image
-    // const nextMangaInfo = nextImage(mangaURL, idxChapter, idxImage);
-    // if (typeof nextMangaInfo === "object") {
-    //   // console.log("BEFORE nextMangaInfo");
-    //   probeImage("probe-next", nextMangaInfo);
-    //   // console.log("AFTER  nextMangaInfo");
-    // }
-    // const previousMangaInfo = previousImage(mangaURL, idxChapter, idxImage);
-    // if (typeof previousMangaInfo === "object") {
-    //   // console.log("BEFORE previousMangaInfo");
-    //   probeImage("probe-previous", previousMangaInfo);
-    //   // console.log("AFTER  previousMangaInfo");
-    // }
+      offsetTop = elemRect.top - bodyRect.top;
+    window.scrollTo({ top: offsetTop, behavior: "smooth" });
   };
-
-  // updateIdxLastChapter = (mangaURL, dict) => {
-  //   const idxLastChapter = dict[KEY_LAST_CHAPTER];
-  //   this.setState({ mangaURL, idxChapter: idxLastChapter });
-  // };
 
   // TODO: Show a progress bar over the current chapter
   render() {
     const { mangaURL, idxChapter, idxImage } = this.state;
-    // console.log("ScanViewer: state:", this.state);
 
-    // if (mangaURL !== this.props.mangaURL) {
-    //   return <WaitingScreen open={true} />;
-    // }
-
-    // if (!(mangaURL !== "" && idxChapter !== null && idxImage !== null)) {
-    //   // return <WaitingScreen open={true} />;
-    //   return null;
-    // } else {
     if (mangaURL !== "" && idxChapter !== null && idxImage !== null) {
-      // const displayedImageProp = this.state.displayedImage;
-      // const offsetXProp = this.state.offsetX;
-      // const inProp = this.state.in;
-      // console.log("displayedImageProp", displayedImageProp);
-      // console.log("in", inProp);
-
       return (
         <React.Fragment>
           {/* <WaitingScreen open={!displayedImageProp} /> */}
           <Button
             style={{
-              // position: "fixed",
               float: "left",
               top: "10px",
               left: "10px",
@@ -266,13 +159,12 @@ class ScanViewer extends React.Component {
           <CSSTransition
             in={true}
             appear={true}
-            timeout={2000}
+            timeout={500}
             classNames="fade"
           >
             <DisplayImage
               mangaInfo={{ mangaURL, idxChapter, idxImage }}
               imageLoaded={this.imageLoaded}
-              // offsetX={offsetXProp}
               ref={this.refImageFrame}
             />
           </CSSTransition>
@@ -283,26 +175,5 @@ class ScanViewer extends React.Component {
     }
   }
 }
-
-// /**
-//  * Allow to get the HTML ref of an image (used to position the image
-//  * on the page).
-//  *
-//  * @param {*} htmlId
-//  * @param {*} mangaInfo
-//  * @param {*} getRef
-//  */
-// function probeImage(htmlId, mangaInfo, getRef) {
-//   // console.log("probeImage mangaInfo:", mangaInfo);
-//   const { mangaURL, idxChapter, idxImage } = mangaInfo;
-//   if (mangaURL !== "" && idxChapter !== null && idxImage !== null) {
-//     ReactDOM.render(
-//       <Box style={{ visibility: "hidden", position: "fixed", top: 0, left: 0 }}>
-//         <DisplayImage {...{ mangaInfo, getRef }} />
-//       </Box>,
-//       document.querySelector("#" + htmlId)
-//     );
-//   }
-// }
 
 export default ScanViewer;
