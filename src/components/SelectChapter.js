66bf21c4cb8d3571800bc39b7c24995fe043b722
frozar <fabien.rozar@gmail.com>
2020-12-11 15:54:59 +0400

    Display chapters' jacket

--- src/components/SelectChapter.js
+++ src/components/SelectChapter.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 
-import { getIdxChapters } from "../db.js";
+import { getIdxChapters, getImagesURL } from "../db.js";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -42,60 +42,75 @@ const useStyles = makeStyles((theme) => ({
 export default function SelectChapter(props) {
   const classes = useStyles();
 
+  const [chaptersJacket, setChaptersJacket] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const tmpChaptersJacket = {};
+      const tmpListIdxChapters = await getIdxChapters(props.path);
+      await Promise.all(
+        tmpListIdxChapters.map(async (idxChapter) => {
+          const imagesURL = await getImagesURL(props.path, idxChapter);
+          tmpChaptersJacket[idxChapter] = imagesURL[0];
+        })
+      );
+      // console.log("tmpChaptersJacket", tmpChaptersJacket);
+      setChaptersJacket(tmpChaptersJacket);
+    }
+    fetchData();
+  }, [props.path]);
+
   const handleOnClick = (event) => {
     event.persist();
     const idxChapter = event.target.getAttribute("value");
     props.selectChapter(idxChapter);
   };
 
-  const renderRange = (listIdxChapters) => {
-    // TODO: make a request to get first image of each chapter
-    return listIdxChapters.map((idx) => {
-      return (
-        <Box
-          style={{
-            flexGrow: "0",
-            flexShrink: "0",
-            flexBasis: "50%",
-            maxWidth: "25%",
-            height: "400px",
-            marginBottom: "3rem",
-            textAlign: "center",
-          }}
-          key={idx}
-        >
-          <Grid
-            item
-            className={classes.backgroundImageThumb}
-            style={{
-              backgroundImage: `url(https://lelscan.net/mangas/${props.path}/${idx}/00.jpg)`,
-            }}
-            value={idx}
-            onClick={handleOnClick}
-          ></Grid>
-          <h3>{idx}</h3>
-        </Box>
-      );
-    });
+  const renderChaptersJacket = () => {
+    return Object.keys(chaptersJacket)
+      .map(Number)
+      .sort(function (a, b) {
+        return a - b;
+      })
+      .reverse()
+      .map((idx) => {
+        if (!chaptersJacket[idx]) {
+          return null;
+        } else {
+          const url = chaptersJacket[idx];
+          return (
+            <Box
+              style={{
+                flexGrow: "0",
+                flexShrink: "0",
+                flexBasis: "50%",
+                maxWidth: "25%",
+                height: "400px",
+                marginBottom: "3rem",
+                textAlign: "center",
+              }}
+              key={idx}
+            >
+              <Grid
+                item
+                className={classes.backgroundImageThumb}
+                style={{
+                  backgroundImage: `url(${url})`,
+                }}
+                value={idx}
+                onClick={handleOnClick}
+              ></Grid>
+              <h3>{idx}</h3>
+            </Box>
+          );
+        }
+      });
   };
 
-  const [listIdxChapters, setListIdxChapters] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const tmpListIdxChapters = await getIdxChapters(props.path);
-      tmpListIdxChapters.reverse();
-      console.log("tmpListIdxChapters", tmpListIdxChapters);
-      setListIdxChapters(tmpListIdxChapters);
-    }
-    fetchData();
-  }, [props.path]);
-
-  // console.log("listIdxChapters", listIdxChapters);
   return (
     <React.Fragment>
       <Grid container className={classes.root} justify="center" spacing={2}>
-        {renderRange(listIdxChapters)}
+        {renderChaptersJacket()}
       </Grid>
     </React.Fragment>
   );
