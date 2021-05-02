95db3682ac2134f3ed2d37e47d16c82be7fb6233
frozar <fabien.rozar@gmail.com>
2021-05-03 01:36:10 +0400

    Bugfix: get back App as a class component: avoid to be unmounted and avoid strange behavior between chapters.

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,89 @@
 import firebase from "../firebase";
 
-import React, { useState } from "react";
+import React from "react";
 import { ThemeProvider } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-import history from "../history";
 import dashify from "dashify";
 
+import history from "../history";
 import theme from "../style/theme";
-
-import "../App.css";
-
+import { getImagesURL, getIdxChapters } from "../db.js"; // getMangas
 import SelectManga from "./SelectManga";
 import SelectChapter from "./SelectChapter";
 import ScanViewer from "./ScanViewer";
-import { getImagesURL, getIdxChapters } from "../db.js";
+
+import "../App.css";
 
 firebase.analytics();
 
-export default function App() {
-  const [state, setState] = useState({
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+// }
+
+// async function scrapRandomChapter() {
+//   const db = await getMangas();
+
+//   const filterDb = {};
+//   for (const [mangaPath, detail] of Object.entries(db)) {
+//     const emptyChapters = [];
+//     for (const [idx, chapterDetail] of Object.entries(detail.chapters)) {
+//       if (chapterDetail.content.length === 0) {
+//         emptyChapters.push(idx);
+//       }
+//     }
+//     if (emptyChapters.length !== 0) {
+//       filterDb[mangaPath] = emptyChapters;
+//     }
+//   }
+//   const keys = Object.keys(filterDb);
+//   const idxManga = getRandomInt(0, keys.length);
+//   const electedMangaPath = keys[idxManga];
+//   const idxChapter = getRandomInt(0, filterDb[electedMangaPath].length);
+//   const electedIdxChapter = filterDb[electedMangaPath][idxChapter];
+
+//   // console.log("scrapRandomChapter electedMangaPath", electedMangaPath);
+//   // console.log("scrapRandomChapter electedIdxChapter", electedIdxChapter);
+//   getImagesURL(electedMangaPath, electedIdxChapter);
+// }
+
+// setInterval(() => {
+//   scrapRandomChapter();
+// }, 1000 * 60 * 3);
+
+export default class App extends React.Component {
+  state = {
     path: undefined,
     title: undefined,
     idxChapter: undefined,
     imagesURL: [],
-  });
+  };
 
-  const selectManga = (title) => {
+  selectManga = (title) => {
     const path = dashify(title);
-    setState({ path, title });
+    this.setState({ path, title });
     history.push("/chapter");
   };
 
-  const selectChapter = async (path, idxChapter) => {
-    setState({ path, idxChapter, imagesURL: [] });
+  selectChapter = async (path, idxChapter) => {
+    this.setState({ path, idxChapter, imagesURL: [] });
     history.push("/reader");
     const imagesURL = await getImagesURL(path, idxChapter);
-    setState({ path, idxChapter, imagesURL });
+    // console.log("[selectChapter] imagesURL", imagesURL);
+    this.setState({ path, idxChapter, imagesURL });
   };
 
-  const previousChapter = async () => {
-    const { path, idxChapter } = state;
+  previousChapter = async () => {
+    const { path, idxChapter } = this.state;
     const idxChapters = await getIdxChapters(path);
     const idx = idxChapters.indexOf(idxChapter);
     if (0 < idx) {
       const idxPreviousChapter = idxChapters[idx - 1];
       const imagesURL = await getImagesURL(path, idxPreviousChapter);
       const idxImage = imagesURL.length - 1;
-      setState({ idxChapter: idxPreviousChapter, imagesURL });
+      this.setState({ idxChapter: idxPreviousChapter, imagesURL });
       return idxImage;
     } else {
       console.info("previousChapter: no more scan");
@@ -55,8 +91,8 @@ export default function App() {
     }
   };
 
-  const nextChapter = async () => {
-    const { path, idxChapter } = state;
+  nextChapter = async () => {
+    const { path, idxChapter } = this.state;
     const idxChapters = await getIdxChapters(path);
     const idx = idxChapters.indexOf(idxChapter);
     const maxIdx = idxChapters.length - 1;
@@ -64,7 +100,7 @@ export default function App() {
       const idxNextChapter = idxChapters[idx + 1];
       const imagesURL = await getImagesURL(path, idxNextChapter);
       const idxImage = 0;
-      setState({ idxChapter: idxNextChapter, imagesURL });
+      this.setState({ idxChapter: idxNextChapter, imagesURL });
       return idxImage;
     } else {
       console.info("nextChapter: no more scan");
@@ -72,40 +108,51 @@ export default function App() {
     }
   };
 
-  const { path, title, idxChapter, imagesURL } = state;
-  return (
-    <Router history={history}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/manga" />
-          </Route>
-          <Route path="/manga" exact>
-            <SelectManga selectManga={selectManga} />
-          </Route>
-          <Route path="/chapter" exact>
-            <SelectChapter
-              selectChapter={selectChapter}
-              path={path}
-              title={title}
-            />
-          </Route>
-          <Route path="/reader" exact>
-            {path === undefined || idxChapter === undefined ? (
+  resetState = () => {
+    this.setState({
+      path: undefined,
+      idxChapter: undefined,
+      imagesURL: [],
+    });
+  };
+
+  render() {
+    const { path, title, idxChapter, imagesURL } = this.state;
+    // console.log("App: state:", { path, title, idxChapter, imagesURL });
+    return (
+      <Router history={history}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Switch>
+            <Route path="/" exact>
               <Redirect to="/manga" />
-            ) : (
-              <ScanViewer
+            </Route>
+            <Route path="/manga" exact>
+              <SelectManga selectManga={this.selectManga} />
+            </Route>
+            <Route path="/chapter" exact>
+              <SelectChapter
+                selectChapter={this.selectChapter}
                 path={path}
-                idxChapter={idxChapter}
-                imagesURL={imagesURL}
-                nextChapter={nextChapter}
-                previousChapter={previousChapter}
+                title={title}
               />
-            )}
-          </Route>
-        </Switch>
-      </ThemeProvider>
-    </Router>
-  );
+            </Route>
+            <Route path="/reader" exact>
+              {path === undefined || idxChapter === undefined ? (
+                <Redirect to="/manga" />
+              ) : (
+                <ScanViewer
+                  path={path}
+                  idxChapter={idxChapter}
+                  imagesURL={imagesURL}
+                  nextChapter={this.nextChapter}
+                  previousChapter={this.previousChapter}
+                />
+              )}
+            </Route>
+          </Switch>
+        </ThemeProvider>
+      </Router>
+    );
+  }
 }
